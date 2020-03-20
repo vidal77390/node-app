4399c401e5ab3b8445b2e3349efa401d593e5bb7
VIDAL Léo <vidalleo@macbook-pro-de-vidal.home>
2020-03-20 15:21:56 +0100

    ajout des test ava

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
+const https = require('https');
+
+function requestCityData(cityName) {
+  return new Promise((resolve, reject) => {
+    https.get(`https://geocode.xyz/${cityName.replace(/ /g, '+')}?json=1`, (res) => {
+      res.on('data', (buffer) => {
+        resolve(JSON.parse(buffer.toString('utf8')));
+      });
+    }).on('error', (e) => {
+      reject(e);
+    });
+  });
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Node-App' });
+  res.render('index', { title: 'Node App' });
+});
+
+router.post('/ville', async function(req, res, next) {
+  const data = await requestCityData(req.body.ville);
+  const longt = Number(data.longt);
+  const latt = Number(data.latt);
+  res.render('ville', {
+    ville: req.body.ville,
+    missingCoords: isNaN(longt) || longt === 0,
+    longt: longt,
+    latt: latt
+  });
 });
 
 module.exports = router;
---
+++ test.js
@@ -0,0 +1,41 @@
+const test = require('ava');
+const http = require('http');
+//const fetch = require('node-fetch');
+
+function httpGet(url) {
+  return new Promise((resolve, reject) => {
+    http.get(url, (res) => {
+      res.on('data', (buffer) => {
+        resolve(buffer.toString('utf8'));
+      });
+    }).on('error', (e) => {
+      reject(e);
+    });
+  });
+}
+
+/*function httpPost(url, body) {
+  return fetch(url, {
+          method: 'post',
+          body:    JSON.stringify(body),
+          headers: { 'Content-Type': 'application/json' },
+      })
+      .then(res => res.text());
+}*/
+
+test('home page should contain a form', async t => {
+  // condition de passage: la page contient un formulaire HTML
+  // plus concretement: la reponse à HTTP `GET /` doit contenir `<form`
+  const html = await httpGet('http://localhost:3000/');
+  t.regex(html, /<form/);
+});
+
+/*test('city page should contain the name of the city', async t => {
+  const html = await httpPost('http://localhost:3000/ville', { ville: 'paris' });
+  t.regex(html, /paris/);
+});
+
+test('city name with accentuated character should be supported', async t => {
+  const html = await httpPost('http://localhost:3000/ville', { ville: 'nîmes' });
+  t.regex(html, /nîmes/);
+});*/
